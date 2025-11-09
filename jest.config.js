e26047704ddb21e4978ceb51cd57982d323d1e9f
OzMaatuk <ozmaatuk@gmail.com>
2025-11-09 02:16:06 +0000

    fixing jest coverage issue

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 // jest.config.js
 
-/* eslint-disable @typescript-eslint/no-require-imports */
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
@@ -9,20 +9,49 @@ const createJestConfig = nextJest({
 
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: 'node',
+
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+
+  testMatch: [
+    '<rootDir>/tests/unit/**/*.test.{js,jsx,ts,tsx}',
+    '<rootDir>/tests/integration/**/*.test.{js,jsx,ts,tsx}',
+  ],
+
+  collectCoverage: true,
+  coverageProvider: 'v8',
+  coverageDirectory: 'coverage',
+
   collectCoverageFrom: [
     'src/**/*.{js,jsx,ts,tsx}',
     '!src/**/*.d.ts',
     '!src/**/*.stories.{js,jsx,ts,tsx}',
     '!src/**/__tests__/**',
+    '!src/app/api/**',
+    '!src/lib/prisma.ts',
   ],
-  testMatch: [
-    '<rootDir>/tests/unit/**/*.test.{js,jsx,ts,tsx}',
-    '<rootDir>/tests/integration/**/*.test.{js,jsx,ts,tsx}',
+
+  coveragePathIgnorePatterns: [
+    '<rootDir>/.next/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/coverage/',
+    '\\.d\\.ts$',
   ],
+
+  // Transform TS/JS
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+
+  // Critical: Transform ESM in node_modules
+  transformIgnorePatterns: [
+    'node_modules/(?!parse5|isomorphic-dompurify|dompurify|jsdom)/', // Only transform these
+  ],
+
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
 };
 
 module.exports = createJestConfig(customJestConfig);
--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,13 @@
 import '@testing-library/jest-dom';
+
+// Mock crypto for jwt.ts (if needed)
+if (!global.crypto) {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  const { webcrypto } = require('crypto');
+  global.crypto = webcrypto;
+}
+
+// eslint-disable-next-line no-undef
+jest.mock('isomorphic-dompurify', () => ({
+  sanitize: (dirty) => dirty.replace(/<script.*?>.*?<\/script>/gi, ''),
+}));
